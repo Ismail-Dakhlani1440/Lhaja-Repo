fad24b21a248f381f7df3e5af1e48f53219bc622
LunaDevX <mouna1.saiss@gmail.com>
2026-01-21 16:04:37 +0100

    burger

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+session_start();
+use App\Modals\DB\Database;
+use App\router\Route;
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/router/Web.php';
+use App\Controller\AuthController;
+
+$path = $_SERVER['PATH_INFO'] ?? '/';
+$method = strtoupper($_SERVER['REQUEST_METHOD']);
+// dd(Route::getRoutes()[$method]);
+
+$isFound = false;
+foreach(Route::getRoutes()[$method] as $route) {
+    if ($path === $route['path']) {
+        $isFound = true;
+        $controllerName = $route['class'];
+        $actionName = $route['action'];
+        $controllerObject = new $controllerName(Database::getInstance());
+        $controllerObject->$actionName();
+    }
+}
+if(!$isFound) {
+    echo 'Not found 404';
+}
