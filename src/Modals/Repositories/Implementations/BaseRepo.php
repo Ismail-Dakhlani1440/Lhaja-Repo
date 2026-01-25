21691c823db4bf9dd65b86e9abdebff2427ea543
LunaDevX <mouna1.saiss@gmail.com>
2026-01-25 01:23:30 +0100

    feat: enhance RecruteurController with CRUD operations and update routing

--- src/Modals/Repositories/Implementations/BaseRepo.php
+++ src/Modals/Repositories/Implementations/BaseRepo.php
@@ -2,10 +2,10 @@
 
 namespace App\Modals\Repositories\Implementations;
 use App\Modals\DB\Database;
-use App\Modals\Repositories\Interfaces\FetchInterface;
-use App\Modals\Repositories\Interfaces\InsertIterface;
-use App\Modals\Repositories\Interfaces\EditInterface;
-use App\Modals\Repositories\Interfaces\DeleteIterface;
+use App\Modals\Repositories\FetchInterface;
+use App\Modals\Repositories\InsertIterface;
+use App\Modals\Repositories\EditInterface;
+use App\Modals\Repositoriesa\DeleteIterface;
 use PDO;
 
 abstract class BaseRepo implements FetchInterface,EditInterface,InsertIterface,DeleteIterface
--- src/router/Web.php
+++ src/router/Web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Controllers\RecruteurController;
 use App\router\Route;
 use App\Controllers\TestController;
 
@@ -22,9 +24,14 @@ Route::get('/dashboardCandidate', TestController::class, 'dashboardCandidate');
 
 Route::get('/JobsRecommandés', TestController::class, 'JobsRecommandés');
 
-Route::get('/dashboardRecruteur', TestController::class, 'dashboardRecruteur');
-
 Route::get('/candidatures', TestController::class, 'candidatures');
+
+// rexcruteur routes
+Route::get('/dashboardRecruteur', RecruteurController::class, 'dashboardRecruteur');
+
+Route::post('/recruteur/add_post', RecruteurController::class, 'addPost');
+Route::post('/recruteur/update_post', RecruteurController::class, 'updatePost');
+Route::post('/recruteur/delete_post', RecruteurController::class, 'deletePost');
 
 
 
